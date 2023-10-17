827264a20295d78785e175f855ed6f8e49acf570
Abhay Gupta <98942880+abhay272129@users.noreply.github.com>
2023-10-17 20:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let rem=0;
+	let output="";
+	while(num>0){
+		rem=num%2;
+		output=rem+output;
+		num=parseInt(num/10);
+	}
+  if(num==1){
+	  output=num+output;
+  }
+	return output;
 }
 
 window.decimalToBinary = decimalToBinary;
